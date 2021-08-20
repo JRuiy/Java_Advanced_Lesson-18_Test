d4cc5db9b15152cc3dc6e97276a99bc299cc3e49
Vadym <vadimhodor@gmail.com>
2021-08-21 02:42:43 +0300

    Test

---
+++ Lesson_18_Test/src/test/java/Lesson_18_Test/Lesson_18_Test/AppTest.java
@@ -0,0 +1,20 @@
+package Lesson_18_Test.Lesson_18_Test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
